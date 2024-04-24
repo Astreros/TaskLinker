09b2c3c04a2b0cb5388d749f8836acaf3a552860
Jonathan Leprini <leprini.jonathan@protonmail.com>
2024-04-24 12:19:58 +0200

    Création de EmployeeType ainsi que la vue du formulaire + Vue des employés + Controller

--- src/Controller/TeamController.php
+++ src/Controller/TeamController.php
@@ -2,25 +2,47 @@
 
 namespace App\Controller;
 
+use App\Form\EmployeeType;
+use App\Repository\EmployeeRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class TeamController extends AbstractController
 {
+    public function __construct(private readonly EntityManagerInterface $entityManager, private EmployeeRepository $employeeRepository)
+    {
+    }
     #[Route('/team', name: 'team.show')]
     public function index(): Response
     {
+        $employees = $this->employeeRepository->findAll();
+
         return $this->render('team/index.html.twig', [
             'pageName' => 'Équipes',
+            'employees' => $employees,
         ]);
     }
 
     #[Route('/team/{id}/edit', name: 'team.edit')]
-    public function editTeam(int $id): Response
+    public function editTeam(int $id, Request $request): Response
     {
+        $employee = $this->employeeRepository->find($id);
+        $formEmployee = $this->createForm(EmployeeType::class, $employee);
+
+        $formEmployee->handleRequest($request);
+
+        if ($formEmployee->isSubmitted() && $formEmployee->isValid()) {
+            $this->entityManager->flush();
+            $this->addFlash('success', 'Modifications enregistrées');
+            return $this->redirectToRoute('team.show');
+        }
+
         return $this->render('team/edit.html.twig', [
-            'pageName' => 'Modifier'
+            'pageName' => 'Modifier',
+            'formEmployee' => $formEmployee,
         ]);
     }
 }
---
+++ src/Form/EmployeeType.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Employee;
+//use App\Entity\Project;
+//use Symfony\Bridge\Doctrine\Form\Type\EntityType;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EmployeeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, [
+                'label' => 'Nom',
+            ])
+            ->add('firstName', TextType::class, [
+                'label' => 'Prénom'
+            ])
+            ->add('email', EmailType::class, [
+                'label' => 'Email',
+            ])
+            ->add('contractType', TextType::class, [
+                'label' => 'Type de contrat',
+            ])
+            ->add('joiningDate', DateType::class, [
+                'label' => 'Date d\'embauche',
+                'widget' => 'single_text',
+            ])
+//            ->add('project', EntityType::class, [
+//                'class' => Project::class,
+//                'choice_label' => 'id',
+//                'multiple' => true,
+//            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Employee::class,
+        ]);
+    }
+}
